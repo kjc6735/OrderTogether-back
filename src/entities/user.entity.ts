261095f4febc0db8a52ac912bd828c2f41aff2c4
kjc6735 <penguin_03@naver.com>
2022-08-28 18:11:13 +0900

    class-validator 추가/ user entity에 class-validator 적용

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,3 +1,4 @@
+import { IsNotEmpty, MaxLength, MinLength } from 'class-validator';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
@@ -5,21 +6,33 @@ export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @IsNotEmpty()
+  @MaxLength(20)
+  @MinLength(5)
+  @Column('varchar', { nullable: false, unique: true })
   userId: string;
 
-  @Column()
+  @IsNotEmpty()
+  @Column('varchar', { nullable: false })
   password: string;
 
-  @Column()
+  @IsNotEmpty()
+  @MaxLength(5)
+  @MinLength(5)
+  @Column('varchar', { nullable: false })
   zonecode: string;
 
-  @Column()
+  @IsNotEmpty()
+  @MaxLength(100)
+  @Column('varchar', { nullable: false })
   addressKo: string;
 
-  @Column()
+  @IsNotEmpty()
+  @MaxLength(100)
+  @Column('varchar')
   addressEn: string;
 
-  @Column()
+  @MaxLength(100)
+  @Column('varchar')
   detail?: string;
 }
